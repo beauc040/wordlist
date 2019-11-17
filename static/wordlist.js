5281b3c848619ed09b4ee3540adaaa39abb7b446
beauc040 <beauc040@umn.edu>
2019-11-16 23:51:23 -0600

    Popups only call the API when empty

--- static/wordlist.js
+++ static/wordlist.js
@@ -6,14 +6,16 @@ async function wordPopup(id) {
             popups[i].style.visibility = "hidden";
     }
 
-    let opts = {
-        method: "GET",
-    };
-    let resp = await fetch("/proxy?word=" + id, opts);
-    let definitions = await resp.json();
-    popup.innerHTML = id + ":<p><ul>";
-    for (let i = 0; i < definitions.length; i++)
-        popup.innerHTML += "<li>" + definitions[i] + "</li>";
-    popup.innerHTML += "</ul>";
+    if (popup.innerHTML = "") {
+        let opts = {
+            method: "GET",
+        };
+        let resp = await fetch("/proxy?word=" + id, opts);
+        let definitions = await resp.json();
+        popup.innerHTML = id + ":<p><ul>";
+        for (let i = 0; i < definitions.length; i++)
+            popup.innerHTML += "<li>" + definitions[i] + "</li>";
+        popup.innerHTML += "</ul>";
+    }
     popup.style.visibility = popup.style.visibility == "visible" ? "hidden" : "visible";
 }
